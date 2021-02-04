c071e65099e05b42801738811145b6f5fec12ace
Jayce Gu <x78jay123@gmail.com>
2021-02-04 14:27:49 -0800

    SignUp and Login

--- app/src/main/java/com/example/instagramapp/MainActivity.java
+++ app/src/main/java/com/example/instagramapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.instagramapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -94,7 +95,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnTransition.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(MainActivity.this, SignupLoginActivity.class);
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/example/instagramapp/SignupLoginActivity.java
+++ app/src/main/java/com/example/instagramapp/SignupLoginActivity.java
@@ -1,4 +1,78 @@
 package com.example.instagramapp;
 
-public class SignupLoginActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
+
+public class SignupLoginActivity extends AppCompatActivity {
+
+    private EditText edtUserNameSignUp, edtPasswordSignUp, edtUsernameLogIn, edtPasswordLogIn;
+    private Button btnSignUp, btnLogIn;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_login_activity);
+
+        edtUserNameSignUp = findViewById(R.id.edtUserNameSignUp);
+        edtPasswordSignUp = findViewById(R.id.edtPasswordSignUp);
+        edtUsernameLogIn = findViewById(R.id.edtUserNameLogIn);
+        edtPasswordLogIn = findViewById(R.id.edtPasswordLogIn);
+
+        btnSignUp = findViewById(R.id.btnSignUp);
+        btnLogIn = findViewById(R.id.btnLogIn);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseUser appUser = new ParseUser();
+                appUser.setUsername(edtUserNameSignUp.getText().toString());
+                appUser.setPassword(edtPasswordSignUp.getText().toString());
+
+                appUser.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            FancyToast.makeText(SignupLoginActivity.this, appUser.get("username") + "is signed up successfully.", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+
+                        } else {
+                            FancyToast.makeText(SignupLoginActivity.this, e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
+
+                        }
+                    }
+                });
+
+            }
+        });
+
+        btnLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ParseUser.logInInBackground(edtUsernameLogIn.getText().toString(), edtPasswordLogIn.getText().toString(), new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+                        if (user != null && e == null) {
+                            FancyToast.makeText(SignupLoginActivity.this, user.get("username") + "is logged in successfully.", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+
+                        } else {
+                            FancyToast.makeText(SignupLoginActivity.this, e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
+
+                        }
+                    }
+                });
+
+            }
+        });
+    }
 }
